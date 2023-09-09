13957fe1296fcb75d3069c1eb76e0f0b92b03c4c
ramteke3yash <ramteke.yashkun@gmail.com>
2023-09-10 01:53:52 +0530

    optimised the leaderboard

---
+++ controllers/premiumFeature.js
@@ -0,0 +1,28 @@
+const User = require("../models/user");
+const Expense = require("../models/expense");
+const sequelize = require("../util/database");
+
+exports.getUserLeaderBoard = async (req, res) => {
+  try {
+    const leaderboardofusers = await User.findAll({
+      attributes: [
+        "id",
+        "name",
+        [sequelize.fn("sum", sequelize.col("expenses.amount")), "total_cost"],
+      ],
+      include: [
+        {
+          model: Expense,
+          attributes: [],
+        },
+      ],
+      group: ["user.id"],
+      order: [["total_cost", "DESC"]],
+    });
+
+    res.status(200).json(leaderboardofusers);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
